f35ce2e36d6b091feb96d9b29e59b337251951cc
Kirill <desving12345@gmail.com>
2017-06-10 13:17:04 +0700

    Developed of calculating amount of deposit

--- main.c
+++ main.c
@@ -10,7 +10,35 @@ int main()
 	scanf("%d", &srok);
 	month = srok / 30;
 
-	
+	if (sum > 1000 && srok <= 365 && srok > 0) 
+	{
+		if (sum <= 100000) 
+		{
+			if (srok <= 30)
+				itog = sum * month / 100 * 90;
+			if (srok <= 120 && srok >= 31)
+				itog = sum * month / 100 * 102;
+			if (srok <= 240 && srok >= 121)
+				itog = sum * month / 100 * 106;
+			if (srok <= 365 && srok >= 241)
+				itog = sum * month / 100 * 112;
+		}
+		else 
+		{
+			if (srok <= 30)
+				itog = sum * month / 100 * 90;
+			if (srok <= 120 && srok >= 31)
+				itog = sum * month / 100 * 103;
+			if (srok <= 240 && srok >= 121)
+				itog = sum * month / 100 * 108;
+			if (srok <= 365 && srok >= 241)
+				itog = sum * month / 100 * 115;
+		}
+	}
+	else
+	{
+		printf("%s\n", "Uncorrect value of term or deposit");
+	}
 
 	getch();
 	return 0;
