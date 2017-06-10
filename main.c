3f363a19afc8428d1a8a9d859e9150bc2098e0ce
Kirill <desving12345@gmail.com>
2017-06-10 13:14:23 +0700

    Developed of data entering

---
+++ main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include <conio.h>
+
+int main()
+{
+	int sum, srok, itog, month;
+	printf("%s", "Enter your deposit\n");
+	scanf("%d", &sum);
+	printf("%s", "Enter your of term of the deposit\n");
+	scanf("%d", &srok);
+	month = srok / 30;
+
+	
+
+	getch();
+	return 0;
+}
